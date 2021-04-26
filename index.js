94f1f5c234d1260848f999609980b5e67d9c13dc
clydotron <bgreen007@cox.net>
2021-04-26 07:34:33 -0700

    refactor

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const express = require('express'); //use this sytnax within node
-const app = express();
+require('./services/passport')
 
-app.get('/', (req,res) => {
-  res.send({ hi: 'snork!' })
-});
+const app = express();
+require('./routes/authRoutes')(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
+
+/*
+https://accounts.google.com/o/oauth2/v2/auth/oauthchooseaccount?
+response_type=code&
+redirect_uri=http%3A%2F%2Flocalhost%3A5000%2Fauth%2Fgoogle%2Fcallback&
+scope=profile%20email&
+client_id=684739951574-is6f0ssrhof3rl82dbd7rfl3fvgda59f.apps.googleusercontent.com&flowName=GeneralOAuthFlow
+*/
